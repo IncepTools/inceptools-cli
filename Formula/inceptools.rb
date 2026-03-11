68650bf5c73adb9e8311c8f910b240fc8268df2e
ishan.singla <is.ishan.singla@gmail.com>
2026-03-11 12:47:11 +0530

    feat: Add multi-architecture support to the Homebrew formula and automate its update in the release workflow.

--- Formula/inceptools.rb
+++ Formula/inceptools.rb
@@ -1,12 +1,32 @@
 class Inceptools < Formula
   desc "A powerful, developer-friendly database migration CLI for Go"
   homepage "https://github.com/IncepTools/inceptools-cli"
-  url "https://github.com/IncepTools/inceptools-cli/releases/download/v{{VERSION}}/inceptools-{{OS}}-{{ARCH}}.tar.gz"
-  sha256 "{{SHA256}}"
+  version "{{VERSION}}"
   license "GPL-3.0"
 
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/IncepTools/inceptools-cli/releases/download/v#{version}/inceptools-darwin-amd64.tar.gz"
+      sha256 "{{SHA256_DARWIN_AMD64}}"
+    else
+      url "https://github.com/IncepTools/inceptools-cli/releases/download/v#{version}/inceptools-darwin-arm64.tar.gz"
+      sha256 "{{SHA256_DARWIN_ARM64}}"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/IncepTools/inceptools-cli/releases/download/v#{version}/inceptools-linux-amd64.tar.gz"
+      sha256 "{{SHA256_LINUX_AMD64}}"
+    else
+      url "https://github.com/IncepTools/inceptools-cli/releases/download/v#{version}/inceptools-linux-arm64.tar.gz"
+      sha256 "{{SHA256_LINUX_ARM64}}"
+    end
+  end
+
   def install
-    bin.install "inceptools"
+    # Rename the platform-specific binary to 'inceptools'
+    bin.install Dir["inceptools-*"][0] => "inceptools"
   end
 
   test do
